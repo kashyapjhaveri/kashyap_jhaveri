6b1448b24ae824ee2afb0c1657535f1c59284dff
kashyapjhaveri <jhaverikashyap1998@gmail.com>
2021-08-09 12:44:24 -0400

    Updated experience

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,14 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            location.pathname.replace(/^\//, "") ===
                 this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+            location.hostname === this.hostname
         ) {
-            var target = $(this.hash);
+            let target = $(this.hash);
             target = target.length
                 ? target
                 : $("[name=" + this.hash.slice(1) + "]");
